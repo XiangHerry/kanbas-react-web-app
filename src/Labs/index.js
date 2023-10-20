0e6231098f36a7af2fe51d0169c34489f97b96a6
Chenyi Xiang <xiang.che@northeastern.edu>
2023-10-20 16:31:59 -0400

    h3

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,6 +1,5 @@
 import Nav from "../Nav";
 import Assignment3 from "./a3";
-import Assignment4 from "./a4";
 import { Routes, Route, Navigate } from "react-router";
 import store from "./store";
 import { Provider } from "react-redux";
@@ -13,7 +12,6 @@ function Labs() {
                 <Routes>
                     <Route path="/" element={<Navigate to="a3" />} />
                     <Route path="a3" element={<Assignment3 />} />
-                    <Route path="a4" element={<Assignment4 />} />
                 </Routes>
             </div>
         </Provider>
